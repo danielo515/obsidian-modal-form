d1c54b7c4a6e60e8b43160af5e9d6141c2309e13
Danielo Rodriguez <rdanielo@gmail.com>
2023-09-07 11:13:59 +0200

    refactor: more convenient API

--- examples/basic template example.md
+++ examples/basic template example.md
@@ -1,7 +1,7 @@
 ---
 <%* 
-const formApi = app.plugins.plugins.obsidianModalForm.api;
-const result = await formApi.exampleForm();
+const modalForm = app.plugins.plugins.obsidianModalForm.api;
+const result = await modalForm.openForm('example-form');
 tR += result.asFrontmatterString();
 -%>
 ---
--- src/API.ts
+++ src/API.ts
@@ -1,11 +1,24 @@
 import { App } from "obsidian";
+
 import { FormDefinition, FormModal } from "./FormModal";
 import FormResult from "./FormResult";
 import { exampleModalDefinition } from "./exampleModalDefinition";
 import ModalFormPlugin from "main";
+import { ModalFormError } from "./utils/Error";
 
 export class API {
+	/**
+	 * Constructor for the API class
+	 * @param {App} app - The application instance
+	 * @param {ModalFormPlugin} plugin - The plugin instance
+	 */
 	constructor(private app: App, private plugin: ModalFormPlugin) { }
+
+	/**
+	 * Opens a modal form with the provided form definition
+	 * @param {FormDefinition} formDefinition - The form definition to use
+	 * @returns {Promise<FormResult>} - A promise that resolves with the form result
+	 */
 	openModalForm(formDefinition: FormDefinition): Promise<FormResult> {
 		return new Promise((resolve) => {
 			new FormModal(
@@ -18,12 +31,34 @@ export class API {
 	exampleForm(): Promise<FormResult> {
 		return this.openModalForm(exampleModalDefinition)
 	}
-	openForm(name: string): Promise<FormResult> {
+
+	/**
+	 * Opens a named form
+	 * @param {string} name - The name of the form to open
+	 * @returns {Promise<FormResult>} - A promise that resolves with the form result
+	 * @throws {ModalFormError} - Throws an error if the form definition is not found
+	 */
+	public namedForm(name: string): Promise<FormResult> {
 		const formDefinition = this.plugin.settings?.formDefinitions.find(form => form.name === name);
 		if (formDefinition) {
 			return this.openModalForm(formDefinition);
 		} else {
-			throw new Error(`Form definition ${name} not found`);
+			throw new ModalFormError(`Form definition ${name} not found`);
+		}
+	}
+
+	/**
+	 * Opens a form with the provided form reference
+	 * @param {string | FormDefinition} formReference - The form reference, either a form name of an existing form or an inline form definition
+	 * @returns {Promise<FormResult>} - A promise that resolves with the form result
+	 * @throws {ModalFormError} - Throws an error if the form reference is not found
+	 */
+	public openForm(formReference: string | FormDefinition): Promise<FormResult> {
+		if (typeof formReference === "string") {
+			return this.namedForm(formReference);
+		} else {
+			return this.openModalForm(formReference);
 		}
 	}
 }
+
