cef1ea23bf200650d94aaf90d99bcad37110941d
Danielo Rodriguez <rdanielo@gmail.com>
2023-09-08 11:55:19 +0200

    chore: stupid formatting

--- src/EditFormView.ts
+++ src/EditFormView.ts
@@ -5,20 +5,20 @@ import FormEditor from './views/FormBuilder.svelte'
 
 export const EDIT_FORM_VIEW = "modal-form-edit-form-view";
 export const FieldTypeReadable: Record<AllFieldTypes, string> = {
-  "text": "Text",
-  "number": "Number",
-  "date": "Date",
-  "time": "Time",
-  "datetime": "DateTime",
-  "toggle": "Toggle",
-  "note": "Note",
-  "slider": "Slider",
-  "select": "Select"
+	"text": "Text",
+	"number": "Number",
+	"date": "Date",
+	"time": "Time",
+	"datetime": "DateTime",
+	"toggle": "Toggle",
+	"note": "Note",
+	"slider": "Slider",
+	"select": "Select"
 } as const;
 
 export type EditFormViewState = {
-  type: 'new-form' | 'edit-form',
-  state?: FormDefinition
+	type: 'new-form' | 'edit-form',
+	state?: FormDefinition
 }
 
 /**
@@ -27,75 +27,81 @@ export type EditFormViewState = {
  * Simple, right?
  */
 export class EditFormView extends ItemView {
-  formState: FormDefinition = { title: '', name: '', fields: [] };
-  editType: 'new-form' | 'edit-form' = 'new-form';
-  formEditor!: FormEditor;
-  constructor(readonly leaf: WorkspaceLeaf, readonly plugin: ModalFormPlugin) {
-    super(leaf);
-  }
+	formState: FormDefinition = { title: 'New form', name: '', fields: [] };
+	editType: 'new-form' | 'edit-form' = 'new-form';
+	formEditor!: FormEditor;
+	constructor(readonly leaf: WorkspaceLeaf, readonly plugin: ModalFormPlugin) {
+		super(leaf);
+	}
 
-  getViewType() {
-    return EDIT_FORM_VIEW;
-  }
+	getViewType() {
+		return EDIT_FORM_VIEW;
+	}
 
-  getDisplayText() {
-    return "Edit form";
-  }
+	getDisplayText() {
+		return "Edit form";
+	}
 
-  async onOpen() {
-    this.formEditor = new FormEditor({
-      target: this.contentEl,
-      props: {
-        fields: this.formState.fields
-      }
-    });
-  }
+	async onOpen() {
+		this.formEditor = new FormEditor({
+			target: this.contentEl,
+			props: {
+				fields: this.formState.fields,
+				onChange: () => {
+					console.log(this.formState)
+					this.app.workspace.requestSaveLayout()
+				}
+			}
+		});
+	}
 
-  renderFields(root: HTMLElement) {
-    root.empty();
-    root.createEl("h4", { text: "Fields" });
-    const rows = root.createDiv();
-    rows.setCssStyles({ display: 'flex', flexDirection: 'column', gap: '10px' });
-    this.formState.fields.forEach(field => {
-      const row = rows.createDiv()
-      row.setCssStyles({ display: 'flex', flexDirection: 'row', gap: '8px' })
-      new Setting(row)
-        .addText(text => {
-          text.setPlaceholder('Field name').setValue(field.name).onChange(value => {
-            field.name = value;
-            this.app.workspace.requestSaveLayout();
-          })
-        })
-        .addText(element => {
-          element.setPlaceholder('Field description').setValue(field.description).onChange(value => {
-            field.description = value;
-            this.app.workspace.requestSaveLayout();
-          })
-        })
-        .addDropdown(element => {
-          element.addOptions(FieldTypeReadable)
-          element.setValue(field.input.type)
-          element.onChange((value) => {
-            field.input.type = value as AllFieldTypes;
-            this.app.workspace.requestSaveLayout();
-          })
-        })
-    })
-  }
+	renderFields(root: HTMLElement) {
+		root.empty();
+		root.createEl("h4", { text: "Fields" });
+		const rows = root.createDiv();
+		rows.setCssStyles({ display: 'flex', flexDirection: 'column', gap: '10px' });
+		this.formState.fields.forEach(field => {
+			const row = rows.createDiv()
+			row.setCssStyles({ display: 'flex', flexDirection: 'row', gap: '8px' })
+			new Setting(row)
+				.addText(text => {
+					text.setPlaceholder('Field name').setValue(field.name).onChange(value => {
+						field.name = value;
+						this.app.workspace.requestSaveLayout();
+					})
+				})
+				.addText(element => {
+					element.setPlaceholder('Field description').setValue(field.description).onChange(value => {
+						field.description = value;
+						this.app.workspace.requestSaveLayout();
+					})
+				})
+				.addDropdown(element => {
+					element.addOptions(FieldTypeReadable)
+					element.setValue(field.input.type)
+					element.onChange((value) => {
+						field.input.type = value as AllFieldTypes;
+						this.app.workspace.requestSaveLayout();
+					})
+				})
+		})
+	}
 
-  async onClose() {
-    this.formEditor.$destroy();
-  }
+	async onClose() {
+		this.formEditor.$destroy();
+	}
 
-  async setState(state: { type: string, state?: FormDefinition }, result: ViewStateResult): Promise<void> {
-    if (state && state.state) {
-      this.formState = state.state;
-      this.editType = state.type as 'new-form' | 'edit-form';
-      this.formEditor.$set({ fields: this.formState.fields })
-    }
-    return super.setState(state, result);
-  }
-  getState(): { type: string, state?: FormDefinition } {
-    return { type: this.editType, state: this.formState };
-  }
+	async setState(state: { type: string, state?: FormDefinition }, result: ViewStateResult): Promise<void> {
+		if (state && state.state) {
+			this.formState = state.state;
+			this.editType = state.type as 'new-form' | 'edit-form';
+			this.formEditor.$set({ fields: this.formState.fields })
+		}
+		return super.setState(state, result);
+	}
+	getState(): { type: string, state?: FormDefinition } {
+		return { type: this.editType, state: this.formState };
+	}
 }
+
+
