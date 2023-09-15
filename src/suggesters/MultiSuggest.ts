fac67ea1199b2db9d27e4df0010e959e939927ab
Danielo Rodriguez <rdanielo@gmail.com>
2023-09-15 18:52:35 +0200

    fix: filtering in multi with uppercase

--- src/suggesters/MultiSuggest.ts
+++ src/suggesters/MultiSuggest.ts
@@ -1,29 +1,29 @@
 import { TextInputSuggest } from "./suggest";
 
 export class MultiSuggest extends TextInputSuggest<string> {
-	content: Set<string>;
+  content: Set<string>;
 
-	constructor(input: HTMLInputElement, content: Set<string>, private onSelect: (value: string) => void) {
-		super(app, input);
-		this.content = content;
-	}
+  constructor(input: HTMLInputElement, content: Set<string>, private onSelect: (value: string) => void) {
+    super(app, input);
+    this.content = content;
+  }
 
-	getSuggestions(inputStr: string): string[] {
-		const lowerCaseInputStr = inputStr.toLowerCase();
-		return [...this.content].filter((content) =>
-			content.contains(lowerCaseInputStr)
-		);
-	}
+  getSuggestions(inputStr: string): string[] {
+    const lowerCaseInputStr = inputStr.toLocaleLowerCase();
+    return [...this.content].filter((content) =>
+      content.toLocaleLowerCase().contains(lowerCaseInputStr)
+    );
+  }
 
-	renderSuggestion(content: string, el: HTMLElement): void {
-		el.setText(content);
-	}
+  renderSuggestion(content: string, el: HTMLElement): void {
+    el.setText(content);
+  }
 
-	selectSuggestion(content: string): void {
-		this.onSelect(content);
-		this.inputEl.value = "";
-		// this.inputEl.trigger("blur");
-		this.inputEl.blur()
-		this.close();
-	}
+  selectSuggestion(content: string): void {
+    this.onSelect(content);
+    this.inputEl.value = "";
+    // this.inputEl.trigger("blur");
+    this.inputEl.blur()
+    this.close();
+  }
 }
