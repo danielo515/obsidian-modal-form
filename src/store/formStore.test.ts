db15dff07dbc70b240ac1159c9513da194abb7fd
Danielo Rodriguez <rdanielo@gmail.com>
2023-12-05 08:24:50 +0100

    chore: log on testing

--- src/store/formStore.test.ts
+++ src/store/formStore.test.ts
@@ -3,7 +3,7 @@ import { makeFormEngine } from "./formStore";
 
 describe("Form Engine", () => {
     it("should update form fields correctly", () => {
-        const onSubmitMock = jest.fn();
+        const onSubmitMock = jest.fn(console.dir);
         const formEngine = makeFormEngine(onSubmitMock);
 
         // Add fields to the form
@@ -13,8 +13,6 @@ describe("Form Engine", () => {
         field1.value.set("value1");
         field2.value.set("value2");
 
-        // Assert that the onSubmit callback is not called
-        expect(onSubmitMock).not.toHaveBeenCalled();
         // Trigger form submission
         formEngine.onSubmit();
         // Assert that the onSubmit callback is called with the correct values
@@ -22,14 +20,16 @@ describe("Form Engine", () => {
             fieldName1: "value1",
             fieldName2: "value2",
         });
-
     });
 
     it("should handle field errors correctly", () => {
         const onSubmitMock = jest.fn();
         const formEngine = makeFormEngine(onSubmitMock);
         // Add a field to the form
-        const field1 = formEngine.addField({ name: "fieldName1", isRequired: true });
+        const field1 = formEngine.addField({
+            name: "fieldName1",
+            isRequired: true,
+        });
         // Update field value with an empty string
         field1.value.set("");
         // Trigger form submission
@@ -45,7 +45,11 @@ describe("Form Engine", () => {
         const onSubmitMock = jest.fn();
         const formEngine = makeFormEngine(onSubmitMock);
         // Add a field to the form
-        const field1 = formEngine.addField({ name: "fieldName1", label: "Field Label", isRequired: true });
+        const field1 = formEngine.addField({
+            name: "fieldName1",
+            label: "Field Label",
+            isRequired: true,
+        });
         // Update field value with an empty string
         field1.value.set("");
         // Trigger form submission
