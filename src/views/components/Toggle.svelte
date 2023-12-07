c77c34017096368b9ab7f6448bb0576826545b35
Danielo Rodriguez <rdanielo@gmail.com>
2023-12-07 23:30:19 +0100

    feat(form-editor): UI to set fields as required

---
+++ src/views/components/Toggle.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let checked = false;
+    export let tabindex: number = 0;
+</script>
+
+<div
+    class="checkbox-container"
+    class:is-enabled={checked}
+    on:click={() => (checked = !checked)}
+>
+    <input type="checkbox" {tabindex} bind:checked />
+</div>
