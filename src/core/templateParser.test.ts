6638c9b83c1f67fde550b3a292f915ece5bb6532
Danielo Rodriguez <rdanielo@gmail.com>
2023-12-09 14:51:06 +0100

    wip: parser parses everything but unfinished

---
+++ src/core/templateParser.test.ts
@@ -0,0 +1,102 @@
+import { parseTemplate, test } from "./templateParser";
+import * as S from 'parser-ts/string'
+import * as E from "fp-ts/Either";
+import { pipe } from "@std";
+
+const inspect = (val: unknown) => {
+    console.dir(val, { depth: 10 });
+    return val;
+}
+const logError = E.mapLeft(console.log);
+
+describe("parseTemplate", () => {
+    it.skip("test", () => {
+        pipe(
+            S.run("al{nam{{e}}")(test),
+            inspect)
+    });
+    it("should parse a single identifier template", () => {
+        const template = "{{name}}";
+        const result = parseTemplate(template);
+        expect(result).toEqual(E.of([{ _tag: "variable", value: "name" }]));
+
+    });
+    it("templates can start with an identifier", () => {
+        const template = "{{name}} is a name";
+        const result = parseTemplate(template);
+        expect(result).toEqual(E.of([
+            { _tag: "variable", value: "name" },
+            { _tag: "text", value: " is a name" },
+        ]));
+    });
+    it("should parse a valid template", () => {
+        const template = "Hello, {{name}}!";
+        const result = parseTemplate(template);
+        logError(result);
+        expect(result).toEqual(E.of(
+            [
+                { _tag: "text", value: "Hello, " },
+                { _tag: "variable", value: "name" },
+                { _tag: "text", value: "!" },
+            ],
+        ));
+    });
+    it("should parse a valid template with several variables", () => {
+        const template = "Hello, {{name}}! You are {{age}} years old.";
+        const result = parseTemplate(template);
+        expect(result).toEqual(E.of(
+            [
+                { _tag: "text", value: "Hello, " },
+                { _tag: "variable", value: "name" },
+                { _tag: "text", value: "! You are " },
+                { _tag: "variable", value: "age" },
+                { _tag: "text", value: " years old." },
+            ],
+        ));
+    });
+
+    it("should allow single braces in a template", () => {
+        const template = "This is code {bla}";
+        const result = parseTemplate(template);
+        expect(result).toEqual(E.of(
+            [
+                { _tag: "text", value: "This is code {bla}" },
+            ],
+        ));
+    })
+    it("should allow single braces in a template even if it has variables", () => {
+        const template = "This is code {bla} {{name}}";
+        const result = parseTemplate(template);
+        expect(result).toEqual(E.of(
+            [
+                { _tag: "text", value: "This is code {bla} " },
+                { _tag: "variable", value: "name" },
+            ],
+        ));
+    })
+
+    it("should return a parse error for an invalid template", () => {
+        const template = "Hey, {{name}!";
+        const result = parseTemplate(template);
+        // inspect(result);
+        E.mapLeft(console.log)(result);
+        expect(E.isLeft(result)).toBe(true);
+        if (E.isLeft(result)) {
+            expect(result.left).toBeDefined();
+        } else {
+            fail("Expected a left value");
+        }
+    });
+
+    it("should allow spaces within open and close braces", () => {
+        const template = "Hey, {{ name }}!";
+        const result = parseTemplate(template);
+        expect(result).toEqual(E.of(
+            [
+                { _tag: "text", value: "Hey, " },
+                { _tag: "variable", value: "name" },
+                { _tag: "text", value: "!" },
+            ],
+        ));
+    })
+});
--- src/core/templateParser.ts
+++ src/core/templateParser.ts
@@ -1,59 +1,56 @@
-import * as O from 'fp-ts/Option';
-import * as E from 'fp-ts/Either';
-type StringSection = { _tag: 'StringSection', value: string, startPos: number, endPos: number }
-type TemplateIdentifier = { _tag: 'TemplateIdentifier', name: string, open: number, close: number }
-type Tokens = StringSection | TemplateIdentifier
-type ParsedTemplate = Tokens[];
+import * as P from 'parser-ts/Parser'
+import { run } from 'parser-ts/code-frame'
+import * as C from 'parser-ts/char'
+import * as S from 'parser-ts/string'
+import { Either, pipe } from '@std';
+type TemplateText = { _tag: 'text', value: string }
+type TemplateVariable = { _tag: 'variable', value: string }
+type Token = TemplateText | TemplateVariable
+type ParsedTemplate = Token[];
 
-function StringSection(startPos: number): StringSection {
-    return { _tag: 'StringSection', value: '', startPos, endPos: startPos }
+function TemplateText(value: string): TemplateText {
+    return { _tag: 'text', value }
 }
-abstract class ParseError {
-    readonly _tag: string = 'ParseError' as const
-    constructor(readonly name: string, protected startPos: number) { }
-}
-class UnclosedSectionError extends ParseError {
-    readonly _tag = 'UnclosedSection' as const
-    static make(startPos: number) {
-        return new UnclosedSectionError('UnclosedSection', startPos)
-    }
+function TemplateVariable(value: string): TemplateVariable {
+    return { _tag: 'variable', value }
 }
 
-interface ParseState {
-    readonly input: string;
-    readonly position: number;
-    peek(offset: number): O.Option<string>
-}
 
-class State implements ParseState {
-    constructor(readonly input: string, readonly position: number) { }
-    peek(offset: number): O.Option<string> {
-        return this.input.length > this.position ? O.fromNullable(this.input[this.position + 1]) : O.none
-    }
-    static make(input: string) {
-        return new State(input, 0)
-    }
-}
-function parseString(state: ParseState): ParseState {
-    const { text, currentSection } = state
-    const nextSection = StringSection(currentSection.endPos + 1)
-    let i = nextSection.startPos
-    while (i < text.length) {
-        if (text[i] === '{' && text[i + 1] === '{') {
-            nextSection.endPos = i - 1
-            break
-        }
-        i++
-    }
-    return { ...state, currentSection: nextSection }
-}
+// === Parsers ===
+type TokenParser = P.Parser<string, Token>
+const open = S.fold([S.string('{{'), S.spaces])
+const close = S.fold([S.spaces, S.string('}}')])
+const identifier = S.many1(C.alphanum)
+const templateIdentifier: TokenParser = pipe(
+    identifier,
+    P.between(open, close),
+    P.map(TemplateVariable)
+)
+
+const EofStr = pipe(
+    P.eof<string>(),
+    P.map(() => ''))
+
+export const OpenorEof = P.either(open, () => EofStr)
+export const test = P.many1Till(P.item<string>(), P.lookAhead(OpenorEof))
+
+const text: TokenParser = pipe(
+    test,
+    P.map((value) => TemplateText(value.join(''))))
+// function parseTemplate(template: string): E.Either<ParseError, ParsedTemplate> {
+const TextOrVariable: TokenParser = P.either(templateIdentifier, () => text)
+
+const Template = pipe(
+    P.many(TextOrVariable),
+    P.apFirst(P.eof()),
+)
 /**
  * Given a template string, parse it into an array of tokens.
  * Templates look like this:
  * "Hello {{name}}! You are {{age}} years old."
  * @param template a template string to convert into an array of tokens or an error
  */
-function parseTemplate(template: string): E.Either<ParseError, ParsedTemplate> {
-    const state = ParseState(template)
-
+export function parseTemplate(template: string): Either<string, ParsedTemplate> {
+    return run((Template), template)
+    // return S.run(template)(P.many(Template))
 }
