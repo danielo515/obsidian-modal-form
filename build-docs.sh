b5c65c00778cf02e0b71a1435b95856561d856c7
Danielo Rodriguez <rdanielo@gmail.com>
2024-01-21 13:49:49 +0100

    Add changelog.md to docs

--- build-docs.sh
+++ build-docs.sh
@@ -3,5 +3,6 @@
 # we need to copy the README.md file to the docs folder and remove
 # the docs/ prefix from the links at build time.
 cp README.md docs/index.md
+cp CHANGELOG.md docs/changelog.md
 sed -i '' -e 's/docs\///g' docs/index.md
 mkdocs build
