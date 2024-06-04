721c023ecc0c948f967610f5ab802abb85c4ef9a
Danielo Rodriguez <rdanielo@gmail.com>
2024-06-04 10:43:29 +0200

    chore: more style options

--- src/views/components/FormRow.svelte
+++ src/views/components/FormRow.svelte
@@ -1,23 +1,29 @@
 <script lang="ts">
-	export let label: string;
-	export let id: string;
-	export let hideLabel = false;
+    export let label: string;
+    export let id: string;
+    export let hideLabel = false;
+    export let inline = false;
 </script>
 
-<div class="field-goup">
-	<label for={id} class:hidden-label={hideLabel}>{label}</label>
-	<slot />
+<div class="field-group" class:inline>
+    <label for={id} class:hidden-label={hideLabel}>{label}</label>
+    <slot />
 </div>
 
 <style>
-	.field-goup {
-		display: flex;
-		flex-direction: column;
-		gap: 0.5rem;
-	}
-	.hidden-label {
-		white-space: nowrap;
-		overflow: hidden;
-		visibility: hidden;
-	}
+    .field-group {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+    }
+    .inline {
+        flex-direction: row;
+        align-items: center;
+        gap: 1rem;
+    }
+    .hidden-label {
+        white-space: nowrap;
+        overflow: hidden;
+        visibility: hidden;
+    }
 </style>
--- src/views/components/Toggle.svelte
+++ src/views/components/Toggle.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
     export let checked = false;
     export let tabindex: number = 0;
+    function handleChange() {
+        checked = !checked;
+    }
 </script>
 
-<div
-    class="checkbox-container"
-    class:is-enabled={checked}
-    on:click={() => (checked = !checked)}
->
+<div class="checkbox-container" class:is-enabled={checked} role="checkbox" on:click={handleChange}>
     <input type="checkbox" {tabindex} bind:checked />
 </div>
