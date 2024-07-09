a30a9ff1d12def22fbbec93eb25f34bfe7987180
Danielo Rodriguez <rdanielo@gmail.com>
2024-07-09 14:15:11 +0200

    feat: logger instance
    chore: better logger

---
+++ src/utils/Logger.ts
@@ -0,0 +1,27 @@
+type Level = "DEBUG" | "INFO" | "WARN" | "ERROR";
+function noop() {}
+export class Logger {
+    private static instance: Logger;
+    #level: Level = "DEBUG";
+    private constructor() {}
+
+    static getInstance(): Logger {
+        if (!Logger.instance) {
+            Logger.instance = new Logger();
+        }
+        return Logger.instance;
+    }
+
+    log(message: string) {
+        console.log(message);
+    }
+    error(...args: unknown[]) {
+        console.error(...args);
+    }
+    get debug() {
+        if (this.#level === "DEBUG") return console.log.bind(console, "[DEBUG]");
+        return noop;
+    }
+}
+
+export const logger = Logger.getInstance();
