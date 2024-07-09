7ef6323e99148d10caf98af2c93098d54a0ee25f
Danielo Rodriguez <rdanielo@gmail.com>
2024-07-09 14:15:54 +0200

    chore: user  logger on formStore

--- src/store/formStore.ts
+++ src/store/formStore.ts
@@ -15,7 +15,6 @@ import type { FieldDefinition } from "src/core/formDefinition";
 import { valueMeetsCondition } from "src/core/input";
 import { type Logger, logger } from "src/utils/Logger";
 import type { Readable, Writable } from "svelte/store";
-import type { Readable, Writable} from "svelte/store";
 import { derived, get, writable } from "svelte/store";
 
 type Rule = { tag: "required"; message: string }; //| { tag: 'minLength', length: number, message: string } | { tag: 'maxLength', length: number, message: string } | { tag: 'pattern', pattern: RegExp, message: string };
